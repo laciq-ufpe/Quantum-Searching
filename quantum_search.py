940dd19e6491afc37ce3f73a674a5f02e8d42c2a
Raafm <raafm@cin.ufpe.br>
2024-08-29 01:11:30 -0300

    add another quantum_search to fit the algorithm in find_d

--- quantum_search.py
+++ quantum_search.py
@@ -2,7 +2,7 @@ from grover import grover_algorithm
 import numpy as np
 
 
-def quantum_search(L, target_indices):
+def quantum_search_on_list(L, target_indices):
     grover_call_counter = 0
     oracle_call_counter = 0
     """L should be a numpy array"""
@@ -23,9 +23,10 @@ def quantum_search(L, target_indices):
         state = grover_algorithm(n,
                                  target_indices= target_indices,
                                  iterations=j)
-        # counter to check "run time"
-        grover_call_counter += 1
-        oracle_call_counter += j
+        if j > 0:   
+            # counter to check "run time"
+            grover_call_counter += 1
+            oracle_call_counter += j
 
         # use amplitudes to determine the probability to choose a specific index i
         probabilities = np.abs(state) ** 2
@@ -34,14 +35,58 @@ def quantum_search(L, target_indices):
         # update the max number of possible grover iterations in next try in case L[i] is not a target value
         m = min(lam*m, np.sqrt(N))
 
+    return i, oracle_call_counter, grover_call_counter
 
+def quantum_search(n, target_indices):
+    # the indices should be able to be represented by the qubits
+    assert np.max(target_indices) < 2**n
+
+    """n is the number of qubits"""
+    grover_call_counter = 0
+    oracle_call_counter = 0
+    
+    N = 2**n
+
+    m = 1 # max number of iterations
+    i = 0 # guess of target index
+    # check: Tight bounds on quantum searching (https://arxiv.org/pdf/quant-ph/9605034)
+    # 'Any value of λ strictly between 1 and 4/3 would do'
+    lam = 6/5
+
+    while i not in target_indices:
+        # 'choose j uniformly at random among the nonnegative integers smaller than m'
+        j = np.random.randint(0,m)
+        # returns the amplitudes of the quantum state after j grover iterations
+        state = grover_algorithm(n,
+                                target_indices= target_indices,
+                                iterations=j)
+            
+        if j > 0:   
+            # counter to check "run time"
+            grover_call_counter += 1
+            oracle_call_counter += j
+        
+        
+        # use amplitudes to determine the probability to choose a specific index i
+        probabilities = np.abs(state) ** 2
+        i = np.random.choice(N, p= probabilities)
+        
+        # update the max number of possible grover iterations in next try in case L[i] is not a target value
+        m = min(lam*m, np.sqrt(N))
 
     return i, oracle_call_counter, grover_call_counter
+
 
 if __name__ == "__main__":
     # 2**3 positions
     L = np.array([0,1,2,3,4,5,6,7])
-    print( quantum_search(L, [3,6]))
+    print( quantum_search_on_list(L, [3,6]))
+    print("---"*60)
+    
     # 2**3 - 2 positions
     L = np.array([0,1,2,3,4])
-    print( quantum_search(L, [3,2]))
+    print( quantum_search_on_list(L, [3,2]))
+    print("---"*60)
+
+    for _ in range(10):
+        print(quantum_search(12,[2,4, 1000]))
